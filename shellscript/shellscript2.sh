28fad17c68cfddb8975ad8eabb5e34a80cde6cf2
lakshmipradeep201198 <lakshmipradeep201198@gmail.com>
2018-03-13 16:11:24 +0530

    shellscript

--- shellscript/shellscript2.sh
+++ shellscript/shellscript2.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 echo "Computer CPU information like processor type, speed : $(lscpu)"
+echo "Compter CPU information like processor type , speed : echo $(sudo dmidecode -t 4)"
 echo "Memory information : $(free -m)"
-
+echo "All available shells : cat /etc/shells"
+echo "Mouse settings : echo $(xinput --list --short)"
+echo "Hard disk information : echo $(sudo dmidecode -t memory)"
+echo "File system (Mounted) : echo $(sudo fdisk -l)"
 
